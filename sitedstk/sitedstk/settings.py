89fb12e89a07ed1f662533f593d3846a75bc2aaa
Walter Santos Filho <wcsantosfilho@gmail.com>
2018-05-09 22:10:31 -0300

    Acerto no settings

--- sitedstk/sitedstk/settings.py
+++ sitedstk/sitedstk/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
 
@@ -124,5 +127,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
-import django_heroku
 django_heroku.settings(locals())
