4298ac1f2f8cd23c1c2c69565a2d428030cb22af
Walter Santos Filho <wcsantosfilho@gmail.com>
2018-05-10 01:08:47 -0300

    Primeiras views

--- sitedstk/srchdstk/urls.py
+++ sitedstk/srchdstk/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:linguagem_id>/', views.detail, name='detail'),
 ]
--- sitedstk/srchdstk/views.py
+++ sitedstk/srchdstk/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from .models import Linguagem
 
 def index(request):
-    return HttpResponse("Hello World. Aplicativo de search dos destaques do git.")
+    search_ling = Linguagem.objects.order_by('linguagem_nome')
+    output = ', '.join([l.linguagem_nome for l in search_ling])
+    return HttpResponse(output)
+
+def detail(request, linguagem_id):
+    return HttpResponse("Esta é a linguagem %s" % linguagem_id)
